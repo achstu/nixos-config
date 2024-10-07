f30f561939394f490b1bf63e65bbd00cd5c73d01
Michał Stachurski <root.stachu@gmail.com>
2024-10-07 09:12:21 +0200

    Pushing to remote after sync

--- sync.sh
+++ sync.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# traced_dirs=("/etc/nixos" "~/.config/{helix,kitty,hypr}")
-
 # Sync /etc/nixos with your git repo
 rsync -av --delete /etc/nixos/ ./nixos/
 
@@ -12,3 +10,4 @@ rsync -av --delete ~/.config/{helix,kitty,hypr} ./config/
 # Add changes and commit
 git add .
 git commit -m "Sync latest config"
+git push
